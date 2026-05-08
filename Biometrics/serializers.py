2c264c907255fd819b388bee09451b224c4e2b61
MostafaOnGithub <ma9654101@gmail.com>
2026-05-08 21:07:22 +0300

    Biometric serializer fix

--- Biometrics/serializers.py
+++ Biometrics/serializers.py
@@ -5,10 +5,10 @@ from .models import BiometricReading
 class BiometricReadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = BiometricReading
-        fields = ['id', 'device', 'heart_rate', 'spo2', 'timestamp']
+        fields = ['id', 'device', 'heart_rate', 'timestamp']
 
 
 class LiveBiometricSerializer(serializers.ModelSerializer):
     class Meta:
         model = BiometricReading
-        fields = ['heart_rate', 'spo2', 'timestamp']
+        fields = ['heart_rate', 'timestamp']
